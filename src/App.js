c0ee4c249f6d1724167dcfd352baf323b4a3678b
Mueid <mrmuyed579@gmail.com>
2022-06-14 21:14:51 +0600

    projects route added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/home/Home';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import Projects from './pages/home/Projects/Projects';
 function App() {
   useEffect(()=>{
     AOS.init({duration: 3000});
@@ -12,6 +13,9 @@ function App() {
   return (
     <div>
      <Routes>
+       <Route path='/' element={<Home></Home>} ></Route>
+       <Route path='projects' element={<Projects></Projects>} ></Route>
+       <Route path='/' element={<Home></Home>} ></Route>
        <Route path='/' element={<Home></Home>} ></Route>
      </Routes>
     </div>
--- src/pages/home/Nav.js
+++ src/pages/home/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
     </>
     return (
         <div >
-            <div class="bg-base-300 flex justify-between lg:px-40">
+            <div class="bg-orange-300 flex justify-between lg:px-40">
                 <div class="navbar-start">
                     <div class="dropdown">
                         <label tabindex="0" class="btn btn-ghost lg:hidden">
